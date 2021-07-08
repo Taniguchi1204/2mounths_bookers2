d0ba2f375c134cb76e5583e20d8c8b6a70403c3b
EC2 Default User <ec2-user@ip-172-31-44-226.ec2.internal>
2021-07-08 09:17:58 +0000

    create remote: true

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -5,14 +5,12 @@ class BookCommentsController < ApplicationController
     book_comment = current_user.book_comments.new(book_comment_params)
     book_comment.book_id = @book.id
     book_comment.save
-    redirect_to request.referer
   end
 
   def destroy
     @book = Book.find(params[:book_id])
     book_comment = current_user.book_comments.find_by(id: params[:id], book_id: @book.id)
     book_comment.destroy
-    redirect_to request.referer
   end
 
   private
